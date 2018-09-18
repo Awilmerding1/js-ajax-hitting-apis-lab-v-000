7ec52f14df87fbe0ab16af4212d144c87a2a9109
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-18 18:20:04 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ function getCommits(el) {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.commit.committer.name + '</strong> - ' + commit.commit.message + + commit.commit.message + '</li>').join('')}</ul>`;
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.commit.committer.name + '</strong> - ' + commit.author.name + + commit.commit.message + '</li>').join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList;
 }
 
